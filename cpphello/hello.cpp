0a75f58e0590a6c45c1a17a4778dd994d5e774d5
Taqreez Ali <taqreez.ali@gmail.com>
2021-12-20 14:14:32 +0000

    docker learnings

---
+++ cpphello/hello.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main()
+{
+    std::string name;
+    std::cout<<"Your name: ";
+    std::cin>>name;
+    std::cout<<"\nHello container world: "<<name<<std::endl;
+
+    return 0;
+}
